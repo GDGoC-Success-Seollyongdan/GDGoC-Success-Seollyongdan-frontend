ade9056a93eae9b503df177a618726c6239fc98b
seoyeonsw <c2c4@naver.com>
2025-01-30 00:42:06 +0900

    [SETTING] 폰트 추가

--- app/src/main/java/com/example/seollyongdan_frontend/core_ui/theme/Type.kt
+++ app/src/main/java/com/example/seollyongdan_frontend/core_ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.example.seollyongdan_frontend.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.seollyongdan_frontend.R
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +18,184 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+// FontFamily
+val pretendardBold = FontFamily(Font(R.font.pretendard_bold, FontWeight.Bold))
+val pretendardSemiBold = FontFamily(Font(R.font.pretendard_semibold, FontWeight.SemiBold))
+val pretendardRegular = FontFamily(Font(R.font.pretendard_regular, FontWeight.Normal))
+
+val h1Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 32.sp,
+    )
+val h1Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 32.sp,
+    )
+val h1Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 32.sp,
+    )
+val h2Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 29.sp,
+    )
+val h2Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 29.sp,
+    )
+val h2Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 29.sp,
+    )
+
+val h3Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 26.sp,
+    )
+val h3Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 26.sp,
+    )
+val h3Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 26.sp,
+    )
+
+val h4Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 23.sp,
+    )
+val h4Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 23.sp,
+    )
+val h4Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 23.sp,
+    )
+
+val h5Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 20.sp,
+    )
+val h5Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 20.sp,
+    )
+val h5Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 20.sp,
+    )
+
+val h6Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 18.sp,
+    )
+val h6Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 18.sp,
+    )
+val h6Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 18.sp,
+    )
+
+val h7Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 16.sp,
+    )
+val h7Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 16.sp,
+    )
+val h7Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 16.sp,
+    )
+
+val b1Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 15.sp,
+    )
+val b1Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 15.sp,
+    )
+val b1Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 15.sp,
+    )
+
+val b2Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 14.sp,
+    )
+val b2Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 14.sp,
+    )
+val b2Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 14.sp,
+    )
+
+val b3Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 13.sp,
+    )
+val b3Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 13.sp,
+    )
+val b3Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 13.sp,
+    )
+
+val b4Bold =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 12.sp,
+    )
+val b4Semi =
+    TextStyle(
+        fontFamily = pretendardSemiBold,
+        fontSize = 12.sp,
+    )
+val b4Regular =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 12.sp,
+    )
