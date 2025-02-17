0ab1e86d8126f6867175299f3eb5efbb2e62ae97
seoyeonsw <c2c4@naver.com>
2025-02-18 05:00:56 +0900

    메인 풀 받고 마무리

--- app/src/main/java/com/example/seollyongdan_frontend/presentation/home/screen/BottomSheetSwitcher.kt
+++ app/src/main/java/com/example/seollyongdan_frontend/presentation/home/screen/BottomSheetSwitcher.kt
@@ -14,15 +14,10 @@ fun BottomSheetSwitcher(
 ){
     when (screen){
         BottomSheetScreen.HOME -> BottomSheetMainScreen(homeViewModel, onSearchClick)
-        BottomSheetScreen.SAFETY -> BottomSheetSafetyScreen(homeViewModel, districtName)
         BottomSheetScreen.REAL_ESTATE -> BottomSheetRealEstateScreen(homeViewModel, districtName)
-        BottomSheetScreen.TRAFFIC -> BottomSheetTrafficScreen(homeViewModel)
         BottomSheetScreen.LIFE -> BottomSheetLifeScreen(homeViewModel, districtName)
         BottomSheetScreen.REVIEW -> BottomSheetReviewScreen(homeViewModel, districtName)
         BottomSheetScreen.SAFETY -> BottomSheetSafetyScreen(homeViewModel, districtName, onSafetyVisualizationClick)
-        BottomSheetScreen.REAL_ESTATE -> BottomSheetRealEstateScreen(homeViewModel)
         BottomSheetScreen.TRAFFIC -> BottomSheetTrafficScreen(homeViewModel, districtName, onTrafficVisualizationClick)
-        BottomSheetScreen.LIFE -> BottomSheetLifeScreen(homeViewModel)
-        BottomSheetScreen.REVIEW -> BottomSheetReviewScreen(homeViewModel)
     }
 }
